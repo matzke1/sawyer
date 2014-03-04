240ca1b6c3a872a6c84de0725017230353170f37
Robb Matzke <matzke1@llnl.gov>
2014-03-04 17:05:25 -0500

    Documentation

--- Sawyer.h
+++ Sawyer.h
@@ -27,7 +27,105 @@
  *      Doxygen, PerlDoc, TeX, etc. The markup language supports calling of C++ functors to transform the text as it is
  *      processed.
  *
- *  <b>Good starting places for reading documentation are the namespaces.</b> */
+ *  <b>Good starting places for reading documentation are the [namespaces](namespaces.html).</b> */
+
+
+/** @defgroup design_goals Library design goals
+ *
+ *  @li The API should be well documented and tested.  Every public symbol is documented with doxygen and includes all
+ *      pertinent information about what it does, how it relates to other parts of the library, restrictions and caveats, etc.
+ *      This kind of information is not evident from the C++ interface itself and is often omitted in other libraries'
+ *      documentation.
+ *  @li The library should be easy to use yet highly configurable. Common things should be simple and terse, but less common
+ *      things should still be possible without significant work. Users should not have to worry about who owns what
+ *      objects--the library uses reference counting pointers to control deallocation.  The library should use a consistent
+ *      naming scheme. It should avoid extensive use of templates since they're a common cause of difficulty for beginners.
+ *  @li The library should be familiar to experienced C++ programmers. It should use the facilities of the C++ language, C++
+ *      idioms, and an object oriented design.  The API should not look like a C library being used in a C++ program, and the
+ *      library should not be concerned about being used in languages that are not object oriented.
+ *  @li The library should be safe to use.  Errors should be handled in clear, recoverable, and predictable ways. The library
+ *      should make every attempt to avoid situations where the user can cause a non-recoverable fault due to misunderstanding
+ *      the API.
+ *  @li Functionality is more important than efficiency. Every attempt is made to have an efficient implementation, but when
+ *      there is a choice between functionality and efficiencey, functionality wins. */
+
+
+/** @defgroup smart_pointers Reference counting smart pointers
+ *
+ *  The library makes extensive use of referencing count smart pointers.  It uses the following paradigm consistently for any
+ *  class that intends to be reference counted.
+ *
+ *  @li The class shall mark all of its normal C++ constructors as having protected access.  This is to prevent users from
+ *      allocating such objects statically or on the stack, yet allowing subclasses to use them.
+ *  @li For each class their shall be defined a "Ptr" type which is the smart pointer type for the class.  Class templates
+ *      define this type as a public member; non-template classes may define it as a public member and/or in the same namespace
+ *      as the class. In the latter case, the type name will be "Ptr" appended to the class name.
+ *  @li The class shall have a static <code>instance</code> method corresponding to each C++ constructor. Each such
+ *      class method takes the same arguments as one of the constructors, allocates and constructs and object with
+ *      <code>new</code>, and returns a <code>Ptr</code>. These methods are usually public.
+ *  @li The class shall have a public destructor only for the sake of the smart pointer.
+ *  @li If a class hierarchy needs virtual constructors they shall be named <code>create</code>.
+ *  @li If a class needs virtual copy constructors they shall be named <code>copy</code>.
+ *  @li The class shall have a factory function corresponding to each public <code>instance</code> method.
+ *  @li Factory functions shall have the same name as the class, but an initial lower-case letter.
+ *
+ *  A simple example:
+ *
+ * @code
+ *  class SomeClass {
+ *      int someData_;           // underscores are used for private data members
+ *
+ *  protected:
+ *      SomeClass(): someData_(0) {}
+ *
+ *      explicit SomeClass(int n): someData_(n) {}
+ *
+ *  public:
+ *      typedef boost::shared_ptr<SomeClass> Ptr;
+ *
+ *      static Ptr instance() {
+ *          return Ptr(new SomeClass);
+ *      }
+ *
+ *      static Ptr instance(int n) {
+ *          return Ptr(new SomeClass(n));
+ *      }
+ *  };
+ *
+ *  SomeClass::Ptr someClass() {
+ *      return SomeClass::instance();
+ *  }
+ *
+ *  SomeClass::Ptr someClass(int n) {
+ *      return SomeClass::instance(n);
+ *  }
+ * @endcode */
+
+
+/** @defgroup class_properties Class properties
+ *
+ *  Class data members that act like user-accessible properties are declared with private access. As with all private data
+ *  members, they end with an underscore.  The class provides a pair of methods for accessing each property--one for reading the
+ *  property and one for modifying the property. Some properties are read-only in which case only the writer is provided.
+ *
+ *  All writer properties return a reference to the object that is modified so that property settings can be chained.  If the
+ *  class uses the reference-counting smart-pointer paradigm, then a pointer to the object is returned instead. (See @ref
+ *  smart_pointers).
+ *
+ * @code
+ *  class SomeClass {
+ *      int someProperty_;
+ *  public:
+ *      int someProperty() const {
+ *          return someProperty_;
+ *      }
+ *      SomeClass& someProperty(int someProperty) {
+ *          someProperty_ = someProperty;
+ *          return *this;
+ *      }
+ *  };
+ * @endcode */
+
 
 /** Name space for the entire library.  All Sawyer functionality except for some C preprocessor macros exists inside this
  * namespace.  Most of the macros begin with the string "SAWYER_". */
