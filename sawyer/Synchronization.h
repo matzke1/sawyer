3fc04393d5e2dd40dafd0bbc4c705aa330c3c843
Robb Matzke <matzke1@llnl.gov>
2015-02-08 17:07:50 -0500

    Multi-threaded synchronization used by the library

---
+++ sawyer/Synchronization.h
@@ -0,0 +1,54 @@
+#ifndef Sawyer_Synchronization_H
+#define Sawyer_Synchronization_H
+
+namespace Sawyer {
+
+/** Tag indicating that an algorithm or API should assume multiple threads.
+ *
+ *  Indicating that multiple threads are present by the use of this tag does not necessarily ensure that the affected algorithm
+ *  or API is completely thread-safe. The alrogihm or API's documentation will expound on the details. */
+struct MultiThreadedTag {};
+
+/** Tag indicating that an algorithm or API can assume only a single thread.
+ *
+ *  This typically means that the algorithm or API will not perform any kind of synchronization itself, but requires that the
+ *  callers coordinate to serialize calls to the algorithm or API. */
+struct SingleThreadedTag {};
+
+// Used internally as a mutex in a single-threaded environment.
+class NullMutex {
+public:
+    void lock() {}
+    void unlock() {}
+    bool try_lock() { return true; }
+};
+
+// Used internally as a lock guard in a single-threaded environment.
+class NullLockGuard {
+public:
+    NullLockGuard(NullMutex) {}
+};
+
+/** Traits for thread synchronization. */
+template<typename SyncTag>
+struct SynchronizationTraits {};
+
+template<>
+struct SynchronizationTraits<MultiThreadedTag> {
+    typedef boost::mutex Mutex;
+    typedef boost::shared_mutex SharedMutex;
+    typedef boost::lock_guard<boost::mutex> LockGuard;
+    typedef boost::lock_guard<boost::shared_mutex> SharedLockGuard;
+};
+
+template<>
+struct SynchronizationTraits<SingleThreadedTag> {
+    typedef NullMutex Mutex;
+    typedef NullMutex SharedMutex;
+    typedef NullLockGuard LockGuard;
+    typedef NullLockGuard SharedLockGuard;
+};
+
+
+} // namespace
+#endif
