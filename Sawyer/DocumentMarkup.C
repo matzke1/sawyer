98a8d52963bf5f980d8f70fc2bd5aea37891c76f
Robb Matzke <matzke1@llnl.gov>
2016-09-19 11:24:07 -0400

    Fixed compiler errors and warnings on various systems

--- Sawyer/DocumentMarkup.C
+++ Sawyer/DocumentMarkup.C
@@ -3,6 +3,7 @@
 #include <boost/algorithm/string/join.hpp>
 #include <boost/algorithm/string/trim.hpp>
 #include <boost/foreach.hpp>
+#include <boost/tuple/tuple.hpp>
 
 namespace Sawyer {
 namespace Document {
