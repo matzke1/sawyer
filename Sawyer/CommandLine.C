f0a159314d17b0d9bc5b08be49e0d35ca7e29f93
Robb Matzke <matzke1@llnl.gov>
2016-10-26 10:28:42 -0400

    Fixed bug in CommandLine
    A switch declaration like this:
    
      sg.insert(Switch("foo")
                .argument("list", listParser(anyParser()))
                .explosiveLists(true))
    
    was not exploding the list into separate std::string arguments, but
    rather leaving them as a single std::vector<std::string>.

--- Sawyer/CommandLine.C
+++ Sawyer/CommandLine.C
@@ -997,7 +997,7 @@ Switch::matchArguments(const std::string &switchString, const Location &endOfSwi
         }
     }
 
-    explode(result);
+    explode(parsedValues);
     guard.cancel();
     result.insert(result.end(), parsedValues.begin(), parsedValues.end());
     return nValuesParsed;
