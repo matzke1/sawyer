0ab9739e2c0ae9bc49a5063507f3fb114a65439c
Robb P. Matzke <matzke1@llnl.gov>
2017-02-09 10:58:50 -0500

    Fixed compile error in Trace.size(Label)

--- Sawyer/Trace.h
+++ Sawyer/Trace.h
@@ -330,7 +330,7 @@ public:
     }
     size_t size(const Label &label) const {
         const Successors &successors = index_[label];
-        return successors.empty() ? 0 : successors.back().n;
+        return successors.empty() ? 0 : successors.back().end;
     }
     /** @} */
 
