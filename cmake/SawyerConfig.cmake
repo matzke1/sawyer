188dad26ff61257e0cd87d8118b4b364e8275d97
Robb Matzke <matzke1@llnl.gov>
2019-03-20 11:52:33 -0400

    Fixed another library name

--- cmake/SawyerConfig.cmake
+++ cmake/SawyerConfig.cmake
@@ -1,2 +1,2 @@
 set(Sawyer_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/include)
-set(Sawyer_LIBRARY ${CMAKE_INSTALL_PREFIX}/lib/liblibsawyer.so)
+set(Sawyer_LIBRARY ${CMAKE_INSTALL_PREFIX}/lib/libsawyer.so)
